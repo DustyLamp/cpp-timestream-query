1a633e4f9849f958ae8b7d5395566453bc1f4d2f
Jerezle <iamjeza@gmail.com>
2021-06-21 16:58:03 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatFormField } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sensor-querier';
+
+  cognitoUrl = ""
+
+  token = "";
+
+  showLogin = true;
+
+  attemptLoginWithCognito(){
+
+  }
+
+
+
+
 }
